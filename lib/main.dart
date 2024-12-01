c38892d84fecc9778febe44d5f08157816131c91
mhmoudGamea <mhmoudgamea@gmail.com>
2024-12-01 21:07:11 +0200

    finish app configuration and getting splash view images

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
 
+import './features/splash/presentation/views/splash_view.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const FruitHub());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const SplashView();
   }
 }
