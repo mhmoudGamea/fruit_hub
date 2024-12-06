2a724d5a30c745b91b40ed61b2fcd3a41b29a0b2
mhmoudGamea <mhmoudgamea@gmail.com>
2024-12-06 08:26:24 +0200

    Add Failure class and AuthRepo with Dartz dependency. for error handling & authentication operations.

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/error/failure.dart';
+
+import '../entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<Either<Failure, UserEntity>> loginUserWithEmailAndPassword(
+      {required String email, required String password});
+}
